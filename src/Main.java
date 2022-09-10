d11c88a825945b399c342e261f11206d72cca0f2
advmarinho <marinho.1983@gmail.com>
2022-09-10 18:35:30 -0300

    Comandos git

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 // https://www.gitkraken.com/learn/git/git-reset
 // Teste de reset
+
+// Git Comandos - git remote rm origin - git remote add "site" - git remote add origin "site"
+// git add . - git commit -m "mensagem do commit"
+// git push -u origin <nome-da-branch> -
+
 public class Main {
 
 	public static void main(String[] args) {
